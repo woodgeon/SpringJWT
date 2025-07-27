5b9b5e15cddc0100c0b71905d5b0d08863751c1b
woodgeon <ad4086@naver.com>
2025-07-27 21:56:29 +0900

    feat: 회원가입 로직 구현

---
+++ src/main/java/com/example/springjwt/service/JoinService.java
@@ -0,0 +1,41 @@
+package com.example.springjwt.service;
+
+import com.example.springjwt.entity.UserEntity;
+import com.example.springjwt.repository.UserRepository;
+import com.example.springjwt.web.dto.JoinDTO;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+
+    private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public JoinService(UserRepository userRepository,  BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public void joinProcess(JoinDTO joinDTO) {
+
+        String username = joinDTO.getUsername();
+        String password = joinDTO.getPassword();
+
+        Boolean isExists = userRepository.existsByUsername(username);
+
+        if (isExists) {
+
+            return;
+        }
+
+        UserEntity data = new UserEntity();
+
+        data.setUsername(username);
+        data.setPassword(bCryptPasswordEncoder.encode(password));
+        data.setRole("ROLE_ADMIN");
+
+        userRepository.save(data);
+    }
+}
