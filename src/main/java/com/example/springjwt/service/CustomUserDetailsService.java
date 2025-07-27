21b5dde39ff703986e967c410be50731471ac1da
woodgeon <ad4086@naver.com>
2025-07-27 22:37:30 +0900

    feat: DB기반 로그인 검증 로직 구현

---
+++ src/main/java/com/example/springjwt/service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.example.springjwt.service;
+
+import com.example.springjwt.entity.UserEntity;
+import com.example.springjwt.repository.UserRepository;
+import com.example.springjwt.web.dto.CustomUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    // 데이터베이스에서 특정 유저 조회 후 리턴
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if (userData != null) {
+
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}
